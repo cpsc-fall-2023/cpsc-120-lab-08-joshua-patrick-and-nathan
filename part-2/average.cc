a2918c87f521d197f95557f4ae9e474eb75bbc22
Josh Navarro <joshpatnav@csu.fullerton.edu>
2023-11-01 11:17:42 -0700

    FInsihed Everything

--- part-2/average.cc
+++ part-2/average.cc
@@ -1,4 +1,7 @@
-// TODO: Add the required header
+// Josh Navarro
+// joshpatnav@csu.fullerton.edu
+// @joshpatnav
+// Partners: @realnathanpierce
 
 #include <iostream>
 #include <string>
@@ -7,10 +10,16 @@
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments{argv, argv + argc};
 
-  // TODO: Validate that there is at least one command line argument.
-  // If not, print an error message and return a non-zero value.
+  int size = (static_cast<int>(arguments.size())) - 1;
 
-  // TODO: Write a for-each loop to sum (add up) all of the command line
+  // XTODO: Validate that there is at least one command line argument.
+  // If not, print an error message and return a non-zero value.
+  if (arguments.size() < 2) {
+    std::cout << "error: you must supply at least one number";
+    return 1;
+  }
+
+  // XTODO: Write a for-each loop to sum (add up) all of the command line
   // arguments.
   // Use a double or float type so that your program preserves fractional
   // values.
@@ -18,14 +27,24 @@ int main(int argc, char* argv[]) {
   // of the arguments vector.
   // Each argument is a std::string. You will need to convert each string into
   // a number with the std::stod or std::stof function.
+  double add_up{0};
+  int index{0};
+  for (std::string& args : arguments) {
+    if (index != 0) {
+      add_up += std::stod(args);
+    }
+    index++;
+  }
 
-  // TODO: After the loop has finished summing the arguments, calculate the
+  // XTODO: After the loop has finished summing the arguments, calculate the
   // average of the values. Recall that the average is the total value divided
   // by the number of values.
+  double avg{add_up / size};
 
-  // TODO: Use cout to print out a message of the form
+  // XTODO: Use cout to print out a message of the form
   // average = *AVERAGE*
   // on its own line.
+  std::cout << "average = " << avg;
 
   return 0;
 }
